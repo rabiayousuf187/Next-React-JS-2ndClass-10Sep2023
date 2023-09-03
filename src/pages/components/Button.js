144c754f74b1c93e55550e79976276fb6e56df1c
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-09-03 12:40:49 +0500

    create Button Text dynamic

--- src/pages/components/Button.js
+++ src/pages/components/Button.js
@@ -1,7 +1,7 @@
-export default function Button (){
+export default function Button ({text}){
     return (
         <>
-            <button>Click me</button>
+            <button>{text}</button>
         </>
 
     )
