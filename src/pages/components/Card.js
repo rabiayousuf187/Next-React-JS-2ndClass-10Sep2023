e961d3ef2947186537aefab03fa63aa5312a4277
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-09-10 11:29:54 +0500

    Card comp. updated with style

---
+++ src/pages/components/Card.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from '@/styles/Card.module.css'
+
+function Card() {
+  return (
+    <>
+     
+      <p className={styles.para_style}>Some details of Card</p>
+    </>
+  )
+}
+
+export default Card
