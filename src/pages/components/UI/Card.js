18825b3d35f3950e9d7a7d0a7bb6be3f0acb521d
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-09-10 10:54:49 +0500

    Card comp. update

--- src/pages/components/UI/Card.js
+++ src/pages/components/UI/Card.js
@@ -1,8 +1,12 @@
-export default function Button ({text}){
-    return (
-        <>
-            <button>{text}</button>
-        </>
+import React from 'react'
 
-    )
+function Card() {
+  return (
+    <>
+      <h1>My Card</h1>
+      <button>Click me</button>
+    </>
+  )
 }
+
+export default Card
